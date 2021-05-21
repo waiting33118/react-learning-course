2088932ed5ea8ec00795d323ab9240f8c1cae09c
Tony Chung <waiting33118@gmail.com>
2021-05-21 22:14:21 +0800

    feat: section 3

---
+++ s3/src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from './ExpenseItem'
+import Card from '../UI/Card'
+import './Expenses.css'
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={props.expenses[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[1].date}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[2].date}
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[3].date}
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+      />
+    </Card>
+  )
+}
+export default Expenses

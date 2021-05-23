2c9490174353dd0bc8d5346dae656fb228f93011
Tony Chung <waiting33118@gmail.com>
2021-05-23 15:52:41 +0800

    feat: s4 assignment

--- s4/src/components/Expenses/Expenses.js
+++ s4/src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from './ExpenseItem'
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter'
 import Card from '../UI/Card'
 import './Expenses.css'
+import { useState } from 'react'
 
 function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState('2020')
+
+  const filterByYearHandler = (year) => setSelectedYear(year)
+
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={selectedYear}
+        onFilterByYear={filterByYearHandler}
+      />
       <ExpenseItem
         date={props.expenses[0].date}
         title={props.expenses[0].title}

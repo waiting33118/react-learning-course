404da94fe186f75ae29d56ce2ed8a9bf592d92af
Tony Chung <waiting33118@gmail.com>
2021-05-29 20:06:15 +0800

    feat: section 10

---
+++ s10/src/components/Login/Login.js
@@ -0,0 +1,143 @@
+import React, {
+  useReducer,
+  useState,
+  useEffect,
+  useContext,
+  useRef
+} from 'react'
+
+import Card from '../UI/Card/Card'
+import classes from './Login.module.css'
+import Button from '../UI/Button/Button'
+import AuthContext from '../../store/AuthContext'
+import Input from '../UI/Input/Input'
+
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') }
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') }
+  }
+  return { value: '', isValid: false }
+}
+
+const passwordReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.trim().length > 6 }
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.trim().length > 6 }
+  }
+  return { value: '', isValid: false }
+}
+
+const Login = () => {
+  // const [enteredEmail, setEnteredEmail] = useState('')
+  // const [emailIsValid, setEmailIsValid] = useState()
+  // const [enteredPassword, setEnteredPassword] = useState('')
+  // const [passwordIsValid, setPasswordIsValid] = useState()
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+  const authCtx = useContext(AuthContext)
+  const [formIsValid, setFormIsValid] = useState(false)
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null
+  })
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null
+  })
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('setTimeout')
+      setFormIsValid(emailState.isValid && passwordState.isValid)
+    }, 500)
+    return () => {
+      console.log('clearTimeout')
+      clearTimeout(identifier)
+    }
+  }, [emailState.isValid, passwordState.isValid])
+
+  const emailChangeHandler = (event) => {
+    // setEnteredEmail(event.target.value)
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
+
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // )
+  }
+
+  const passwordChangeHandler = (event) => {
+    // setEnteredPassword(event.target.value)
+    dispatchPassword({ type: 'USER_INPUT', val: event.target.value })
+
+    // setFormIsValid(
+    //   emailState.isValid && event.target.value.trim().length > 6
+    // )
+  }
+
+  const validateEmailHandler = () => {
+    // setEmailIsValid(emailState.isValid)
+    dispatchEmail({ type: 'INPUT_BLUR' })
+  }
+
+  const validatePasswordHandler = () => {
+    // setPasswordIsValid(passwordState.value.trim().length > 6)
+    dispatchPassword({ type: 'INPUT_BLUR' })
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value)
+      return
+    }
+    if (!emailState.isValid) {
+      emailInputRef.current.focus()
+      return
+    }
+    if (!passwordState.isValid) {
+      passwordInputRef.current.focus()
+    }
+  }
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <Input
+          ref={emailInputRef}
+          type="email"
+          id="email"
+          label="E-mail"
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+          isValid={emailState.isValid}
+        />
+        <Input
+          ref={passwordInputRef}
+          type="password"
+          id="password"
+          label="Password"
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+          isValid={passwordState.isValid}
+        />
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default Login
---
+++ s10/src/store/AuthContext.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogin: (email, password) => {},
+  onLogout: () => {}
+})
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const storedUserLoggedIn = localStorage.getItem('isLoggedIn')
+
+    if (storedUserLoggedIn === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext

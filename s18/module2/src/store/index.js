244b9c69a5ba23619fae2eafe5f53a481208936a
Tony Chung <waiting33118@gmail.com>
2021-06-06 09:03:54 +0800

    feat: section 18

---
+++ s18/module2/src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counter from './counter'
+import auth from './auth'
+
+const store = configureStore({
+  reducer: {
+    counter,
+    auth
+  }
+})
+
+export default store
